63d2787a43c9dc697b1d6ce776381c5226d2c2fb
Max Basescu <maxrbasescu@gmail.com>
2015-07-15 12:37:19 -0400

    Wrote main trial functionality and fleshed out interrupt methods

--- TrainingRig_UserInput.py
+++ TrainingRig_UserInput.py
@@ -4,8 +4,8 @@
 # Takes user inputted details 
 import math
 import time
+import random
 import RPi.GPIO as GPIO
-import serial
 import Adafruit_CharLCD as LCD
 import UserInput as UINPUT
 
@@ -30,17 +30,34 @@ lcd.clear()
 # Initialization of current angle, theta
 theta = 0
 
+# Number of encoder readings per revolution
+thetaLap = 8192
+
 # Triggered by an interrupt from pin A, this function modifies
 # the value of theta according to the encoder state
 def readEncoderA(ch):
 	global theta
-	theta += 1 # TEMP
+	
+	# Check direction of encoder turning
+	if GPIO.input(encoderPinB) == False
+		theta += 1 #CW
 
 # Triggered by an interrupt from pin B, this function modifies
 # the value of theta according to the encoder state
 def readEncoderB(ch):
 	global theta
-	theta -= 1 # TEMP
+
+	# Check direction of encoder turning 
+	if GPIO.input(encoderPinA) == False
+		theta -= 1 # CCW
+
+# Converts encoder angle to degrees
+def toDeg(ang):
+	return ang / thetaLap * 360
+
+# Converts degrees to encoder angle
+def toEnc(ang):
+	return ang / 360 * thetaLap
 
 # Attach rising edge interrupts to pins A and B
 GPIO.add_event_detect(encoderPinA, GPIO.RISING, callback=readEncoderA)
@@ -61,15 +78,13 @@ print('dTheta0: ' + str(dTheta0))
 dTheta1 = UINPUT.genForm(lcd, 'dTheta1', 0)
 print('dTheta1: ' + str(dTheta1))
 
+lcd.clear()
 
 # Stop showing cursor on screen
 lcd.show_cursor(False)
 
-# Number of encoder readings per revolution
-thetaLap = 8192
-
 # Duration of motor per feeding
-pulseDur = 2
+pulseDur = 2000 # ms
 
 # Local base path to log folder
 logBase = 'logs/'
@@ -80,36 +95,55 @@ f = open(fName, "a")
 
 # Prints info, then converts values in degrees into values compatible with encoder readings
 f.write('\nglobal | dTheta0={},dTheta1={},pulseDur={},goal={}'.format(dTheta0, dTheta1, pulseDur, goal))
-dTheta0 *= thetaLap / 360
-dTheta1 *= thetaLap / 360
+dTheta0 = toEnc(dTheta0)
+dTheta1 = toEnc(dTheta1)
 goal *= thetaLap
 
 # Writes general trial info including timestamp
 f.write('\nglobal | rat={},day={}\n'.format(ratNum,day))
 f.write('\nglobal | year={},month={},date={},hour={},min={},sec={}\n'.format(time.strftime('%Y'),time.strftime('%m'),time.strftime('%d'),time.strftime('%H'),time.strftime('%M'),time.strftime('%S')))
 
-# Sets initial current lap and angle of last drop
-currLap = 0
-angPrev = 0
-pulseDur = 2 # Seconds
-
 # Begin running actual training program:
+isFeeding = False
+feedStart = 0 # Contains the time at which the motor was started
+nextFeedAng = 0 # Stores next angle at which to begin feeding
+tInit = time.time() # Establish offset time
 
 # Until the rat has run the specified number of laps
-#while absTheta < goal
-#
-#	angle = 0 # TEMP, replace with real input 
-#	
-#	# If the current angle is within three degrees of the target angle
-#	if math.fmod(angle, dTheta0) <= 3 && abs(angPrev - angle) > dTheta0/2
-#		# Store the previous angle
-#		angPrev = angle
-#
-#		# Send a 2 second pulse to the motor
-#		pulseMotor(pulseDur)
-#		
-#	 # currLap = math.floor(angle / thetaLap)
+while theta < goal:
+	tCurr = int(math.floor((time.time() - tInit) * 1000)) # Grabs current time in milliseconds
+	print('data | time={},theta={}\n'.format(tCurr, theta))
 	
+	# Arrived at feeding location
+	if (!isFeeding && theta >= nextFeedAng):
+		feedStart = tCurr
+		isFeeding = True
+		
+		# Begin pulsing motor
+		GPIO.output(ledPin, True)
+		GPIO.output(motorPin, True)
+
+		# Write enable to log
+		f.write('event | time={},feed=1\n'.format(tCurr))
+
+	# End of feeding period
+	elif isFeeding and tCurr - feedStart >= pulseDur
+		# Stop pulsing motor
+		GPIO.output(ledPin, False)
+		GPIO.output(motorPin, False)
+
+		# Calculate current interval between feedings and next feed angle
+		dTheta = dTheta0 + (dTheta1 - dTheta0) * theta / goal
+		nextFeedAng = theta + dTheta + random.randint(-455, 455)
+	
+		# Write disable to log
+		f.write('event | time={},feed=0\n'.format(tCurr))	
+
+	time.sleep(0.01) # Wait 10 ms
+
+# Update time and write end of session to file
+tCurr = int(math.floor((time.time() - tInit) * 1000))
+f.write('event | time={},goal=1\n#\n', tCurr)
 
 f.close()
 
